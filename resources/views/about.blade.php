5250d5f4dd6510624125325a476afe08530bc8f2
Jody Prianggara <jprianggara@gmail.com>
2026-06-05 16:24:15 +0700

    Initial commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,125 @@
+@extends('layouts.main')
+
+@section('front-end')
+				<x-front-navbar></x-front-navbar>
+
+				<section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/bg_4.jpg'); width: 100%">
+								<div class="overlay"></div>
+								<div class="container">
+												<div class="row no-gutters slider-text js-fullheight align-items-end justify-content-center">
+																<div class="col-md-9 ftco-animate pb-5 text-center">
+																			
+																																			
+																			<h1 class="mb-10 bread text-white">About Us</h1>
+																</div>
+												</div>
+								</div>
+				</section>
+
+				<section class="ftco-section ftco-about ftco-no-pt img">
+								<div class="container">
+												<div class="row d-flex">
+																<div class="col-md-12 about-intro">
+																				<div class="row">
+																								<div class="col-md-6 d-flex align-items-stretch">
+																												<div class="img d-flex w-100 align-items-center justify-content-center"
+																																style="background-image:url(images/about-1.jpg);">
+																												</div>
+																								</div>
+																								<div class="col-md-6 pl-md-5 py-5">
+																												<div class="row justify-content-start pb-3">
+																																<div class="col-md-12 heading-section ftco-animate">
+																																				<span class="subheading">About Us</span>
+																																				<h2 class="mb-4">Langit Aksara Tour And Travel</h2>
+																																				<p>Langit" melambangkan kebebasan, ketinggian, dan tujuan perjalanan. "Aksara" merujuk pada tulisan, data, atau informasi. Nama ini mengisyaratkan bahwa aplikasi ini adalah "tulisan" atau "kode" yang membuka gerbang menuju langit, mempermudah akses ke segala penjuru dunia. Ini juga bisa diartikan sebagai "jejak informasi di angkasa," menunjukkan bahwa setiap tiket adalah sebuah catatan perjalanan.</p>
+																																</div>
+																												</div>
+																								</div>
+																				</div>
+																</div>
+												</div>
+								</div>
+				</section>
+
+				
+																				
+				
+
+				<footer class="ftco-footer bg-bottom ftco-no-pt" style="background-image: url(images/bg_3.jpg);">
+								<div class="container">
+												<div class="row mb-5">
+																<div class="col-md pt-5">
+																				<div class="ftco-footer-widget pt-md-5 mb-4">
+																								<h2 class="ftco-heading-2">About</h2>
+																								<p>Langit" melambangkan kebebasan, ketinggian, dan tujuan perjalanan. 
+																									"Aksara" merujuk pada tulisan, data, atau informasi. Nama ini mengisyaratkan bahwa aplikasi ini adalah "tulisan" atau "kode" yang membuka gerbang menuju langit, 
+																									mempermudah akses ke segala penjuru dunia. Ini juga bisa diartikan sebagai "jejak informasi
+																									 di angkasa," menunjukkan bahwa setiap tiket adalah sebuah catatan perjalanan.</p>
+																								<ul class="ftco-footer-social list-unstyled float-md-left float-lft">
+																												<li class="ftco-animate"><a href="https://twitter.com/ConfidoTM" target="_blank"><span
+																																								class="fa fa-twitter"></span></a></li>
+																												<li class="ftco-animate"><a href="https://www.facebook.com/people/Moses-Ramadhan/pfbid02LBSewxdb3wHhiFYTCbMrsoF7MYNC1fq3orHtpquFtnPkcSV8CtrYxLscysnYKaabl/?locale=id_ID" target="_blank"><span
+																																								class="fa fa-facebook"></span></a></li>
+																												<li class="ftco-animate"><a href="https://www.instagram.com/confidotm/?hl=id"
+																																				target="_blank"><span class="fa fa-instagram"></span></a></li>
+																								</ul>
+																				</div>
+																</div>
+																<div class="col-md pt-5 border-left">
+																				<div class="ftco-footer-widget pt-md-5 mb-4">
+																								<h2 class="ftco-heading-2">Maskapai</h2>
+																								<ul class="list-unstyled">
+																												<li><a class="py-2 d-block">Garuda Indonesia</a></li>
+																												<li><a class="py-2 d-block">Citilink</a></li>
+																												<li><a class="py-2 d-block">Batik Air</a></li>
+																												<li><a class="py-2 d-block">Lion Air</a></li>
+																												<li><a class="py-2 d-block">Sriwijaya Air</a></li>
+																												<li><a class="py-2 d-block">Air Asia</a></li>
+																								</ul>
+																				</div>
+																</div>
+																<div class="col-md pt-5 border-left">
+																				<div class="ftco-footer-widget pt-md-5 mb-4">
+																								<h2 class="ftco-heading-2">Have a Questions?</h2>
+																								<div class="block-23 mb-3">
+																												<ul> 
+																																<li><span class="icon fa fa-map-marker"></span><span class="text">Jl.Raya Kaliabang No.8, Perwira, Kec. Bekasi Utara, Kota Bks, Jawa Barat 17122</span></li>
+																																<li><a href="https://wa.me/6281384002161?text=Ingin%20Melaporkan%20Masalah"><span
+																																												class="icon fa fa-whatsapp"></span><span class="text">08123456789 Admin
+																																												1</span></a></li>
+																																<li><a href="https://wa.me/6288219983235?text=Ingin%20Melaporkan%20Masalah"><span
+																																												class="icon fa fa-whatsapp"></span><span class="text">088219983235 Admin
+																																												2</span></a></li>
+																															
+																																												
+																												</ul>
+																								</div>
+																				</div>
+																</div>
+												</div>
+												<div class="row">
+																<div class="col-md-12 text-center">
+
+																				<p>
+																								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+																								Langit Aksara &copy;
+																								<script>
+																												document.write(new Date().getFullYear());
+																								</script>
+																								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+																				</p>
+																</div>
+												</div>
+								</div>
+				</footer>
+
+
+
+				<!-- loader -->
+				<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+												<circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4"
+																stroke="#eeeeee" />
+												<circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4"
+																stroke-miterlimit="10" stroke="#F96D00" />
+								</svg></div>
+@endsection
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,153 @@
+@extends('layouts.main')
+
+@section('front-end')
+				<x-front-navbar></x-front-navbar>
+
+				<section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/bg_1.jpg');">
+								<div class="overlay"></div>
+								<div class="container">
+												<div class="row no-gutters slider-text js-fullheight align-items-end justify-content-center">
+																<div class="col-md-9 ftco-animate pb-5 text-center">
+																				<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i
+																																				class="fa fa-chevron-right"></i></a></span> <span>Contact us <i
+																																class="fa fa-chevron-right"></i></span></p>
+																				<h1 class="mb-0 bread">Contact us</h1>
+																</div>
+												</div>
+								</div>
+				</section>
+
+				<section class="ftco-section ftco-no-pb contact-section mb-4">
+								<div class="container">
+												<div class="row d-flex contact-info">
+																<div class="col-md-3 d-flex">
+																				<div class="align-self-stretch box p-4 text-center">
+																								<div class="icon d-flex align-items-center justify-content-center">
+																												<span class="fa fa-map-marker"></span>
+																								</div>
+																								<h3 class="mb-2">Address</h3>
+																								<p>Jl. Kaliabang No.8, Perwira, Kec. Bekasi Utara, Kota Bks, Jawa Barat 17122</p>
+																				</div>
+																</div>
+																<div class="col-md-3 d-flex">
+																				<div class="align-self-stretch box p-4 text-center">
+																								<div class="icon d-flex align-items-center justify-content-center">
+																												<span class="fa fa-phone"></span>
+																								</div>
+																								<h3 class="mb-2">Contact Number</h3>
+																								<p><a href="https://wa.me/6281384002161?text=Ingin%20Melaporkan%20Masalah">081384002161 Admin
+																																1</a></p>
+																								<p><a href="https://wa.me/6288219983235?text=Ingin%20Melaporkan%20Masalah">088219983235 Admin
+																																2</a></p>
+																				</div>
+																</div>
+																<div class="col-md-3 d-flex">
+																				<div class="align-self-stretch box p-4 text-center">
+																								<div class="icon d-flex align-items-center justify-content-center">
+																												<span class="fa fa-paper-plane"></span>
+																								</div>
+																								<h3 class="mb-2">Email Address</h3>
+																								<p><a href="mailto:Langit Askara@gmail.com">LangitAskara81@gmail.com</a></p>
+																				</div>
+																</div>
+																<div class="col-md-3 d-flex">
+																				<div class="align-self-stretch box p-4 text-center">
+																								<div class="icon d-flex align-items-center justify-content-center">
+																												<span class="fa fa-globe"></span>
+																								</div>
+																								<h3 class="mb-2">Website</h3>
+																								<p><a href="https://confido.fikridean.my.id/">confido.fikridean.my.id</a></p>
+																				</div>
+																</div>
+												</div>
+								</div>
+				</section>
+
+				<section class="ftco-section contact-section ftco-no-pt mt-2">
+								<div class="container">
+												<div class="row block-9">
+																<div class="col-md-12 d-flex justify-content-center">
+																				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4113.43757879172!2d107.01313277851376!3d-6.205613518646044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698951a1c56c9f%3A0xba614a42161711ad!2sBSI%20Square%20Kaliabang!5e0!3m2!1sid!2sid!4v1749365990175!5m2!1sid!2sid" 
+																					width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+																</div>
+												</div>
+								</div>
+				</section>
+
+
+				<footer class="ftco-footer bg-bottom ftco-no-pt" style="background-image: url(images/bg_3.jpg);">
+								<div class="container">
+												<div class="row mb-5">
+																<div class="col-md pt-5">
+																				<div class="ftco-footer-widget pt-md-5 mb-4">
+																								<h2 class="ftco-heading-2">About</h2>
+																								<p>Langit" melambangkan kebebasan, ketinggian, dan tujuan perjalanan. "Aksara" merujuk pada tulisan, data, atau informasi. 
+																									Nama ini mengisyaratkan bahwa aplikasi ini adalah "tulisan" atau "kode" yang membuka gerbang menuju langit, mempermudah
+																									 akses ke segala penjuru dunia. Ini juga bisa diartikan sebagai "jejak informasi di angkasa," 
+																									 menunjukkan bahwa setiap tiket adalah sebuah catatan perjalanan..</p>
+																													<ul class="ftco-footer-social list-unstyled float-md-left float-lft">
+																												<li class="ftco-animate"><a href="https://twitter.com/ConfidoTM" target="_blank"><span
+																																								class="fa fa-twitter"></span></a></li>
+																												<li class="ftco-animate"><a href="https://www.facebook.com/people/Moses-Ramadhan/pfbid02LBSewxdb3wHhiFYTCbMrsoF7MYNC1fq3orHtpquFtnPkcSV8CtrYxLscysnYKaabl/?locale=id_ID" target="_blank"><span
+																																								class="fa fa-facebook"></span></a></li>
+																												<li class="ftco-animate"><a href="https://www.instagram.com/confidotm/?hl=id"
+																																				target="_blank"><span class="fa fa-instagram"></span></a></li>
+																								</ul>
+																								
+																				</div>
+																</div>
+																<div class="col-md pt-5 border-left">
+																				<div class="ftco-footer-widget pt-md-5 mb-4">
+																								<h2 class="ftco-heading-2">Maskapai</h2>
+																								<ul class="list-unstyled">
+																												<li><a class="py-2 d-block">Garuda Indonesia</a></li>
+																												<li><a class="py-2 d-block">Citilink</a></li>
+																												<li><a class="py-2 d-block">Batik Air</a></li>
+																												<li><a class="py-2 d-block">Lion Air</a></li>
+																												<li><a class="py-2 d-block">Sriwijaya Air</a></li>
+																												<li><a class="py-2 d-block">Air Asia</a></li>
+																								</ul>
+																				</div>
+																</div>
+																<div class="col-md pt-5 border-left">
+																				<div class="ftco-footer-widget pt-md-5 mb-4">
+																								<h2 class="ftco-heading-2">Have a Questions?</h2>
+																								<div class="block-23 mb-3">
+																												<ul>
+																																<li><span class="icon fa fa-map-marker"></span><span class="text">Jl. Kaliabang No.8, Perwira, Kec. Bekasi Utara, Kota Bks, Jawa Barat 17122</span></li>
+																																<li><a href="tel://+6281384002161"><span class="icon fa fa-whatsapp"></span><span
+																																												class="text">081384002161 Admin 1</span></a></li>
+																																<li><a href="tel://+6281384002161"><span class="icon fa fa-whatsapp"></span><span
+																																												class="text">088219983235 Admin 2</span></a></li>
+																																
+																												</ul>
+																								</div>
+																				</div>
+																</div>
+												</div>
+												<div class="row">
+																<div class="col-md-12 text-center">
+
+																				<p>
+																								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+																								Langit Aksara &copy;
+																								<script>
+																												document.write(new Date().getFullYear());
+																								</script>
+																								<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+																				</p>
+																</div>
+												</div>
+								</div>
+				</footer>
+
+
+
+				<!-- loader -->
+				<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+												<circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4"
+																stroke="#eeeeee" />
+												<circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4"
+																stroke-miterlimit="10" stroke="#F96D00" />
+								</svg></div>
+@endsection
